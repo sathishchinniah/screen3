54932358de811f25b9a5c381be62348a3a533692
sathish chinniah <sathish73ec@gmail.com>
2016-02-16 13:04:34 +0530

    Initial Commit

---
+++ pocimy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pocimy
+//
+//  Created by Sathish Chinniah on 16/02/16.
+//  Copyright © 2016 Sathish Chinniah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pocimy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pocimy
+//
+//  Created by Sathish Chinniah on 16/02/16.
+//  Copyright © 2016 Sathish Chinniah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
